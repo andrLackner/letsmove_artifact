670022d7690742425c7e3392774e934bfb21070b
Andreas Lackner <andreas.lackner@tuwien.ac.at>
2025-06-19 21:07:41 +0200

    Add ERC20 for IRM and ORIGINAL

---
+++ evaluation/original/test/erc20/ERC20MV.test.js
@@ -0,0 +1,34 @@
+const { BN } = require("@openzeppelin/test-helpers");
+const { expect } = require("chai");
+
+const ERC20MV = artifacts.require("ERC20MV");
+
+contract("ERC20MV", function (accounts) {
+  const [initialHolder, recipient, anotherAccount] = accounts;
+
+  const name = "My Token";
+  const symbol = "MTKN";
+
+  const initialSupply = new BN(100);
+
+  beforeEach(async function () {
+    this.token = await ERC20MV.new(name, symbol, initialHolder, initialSupply);
+  });
+
+  describe("_transfer", function () {
+    it("should transfer tokens", async function () {
+      await this.token.transferInternal(
+        initialHolder,
+        recipient,
+        initialSupply
+      );
+      expect(await this.token.balanceOf(initialHolder)).to.be.bignumber.equal(
+        "0"
+      );
+
+      expect(await this.token.balanceOf(recipient)).to.be.bignumber.equal(
+        initialSupply
+      );
+    });
+  });
+});
---
+++ evaluation/original/test/erc20/ERCCoin.test.js
@@ -0,0 +1,37 @@
+const { expect } = require("chai");
+
+const BasicCoin = artifacts.require("ERCCoin");
+const BasicCoinTest = artifacts.require("ERCCoinTest");
+
+contract("ERCCoin", function (accounts) {
+  const [deployer, user1, user2] = accounts;
+
+  beforeEach(async function () {
+    this.basicCoin = await BasicCoin.new({ from: deployer });
+    this.basicCoinTest = await BasicCoinTest.new(this.basicCoin.address, {
+      from: deployer,
+    });
+
+    await this.basicCoin.register({ from: user1 });
+
+    await this.basicCoin.register({ from: user2 });
+
+    await this.basicCoinTest.register(user1, { from: user1 });
+
+    expect(await this.basicCoin.getBalance(user1)).to.be.bignumber.equal("0");
+    expect(await this.basicCoin.getBalance(user2)).to.be.bignumber.equal("0");
+
+    await this.basicCoin.mintTo("10000", this.basicCoinTest.address, {
+      from: deployer,
+    });
+  });
+
+  describe("when everything is set up", function () {
+    it("should allow to transfer", async function () {
+      let result = await this.basicCoinTest.transfer(user1, "1", user2, {
+        from: user1,
+      });
+      console.log("Transfer cost: ", result.receipt.gasUsed);
+    });
+  });
+});
